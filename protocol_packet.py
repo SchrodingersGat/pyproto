f719526670424896f14a438fdcf229598abe106c
Oliver Walters <oliver.henry.walters@gmail.com>
2017-09-07 17:24:12 +1000

    Packet now inherits from struct

--- protocol_packet.py
+++ protocol_packet.py
@@ -1,21 +1,13 @@
+from protocol_struct import ProtocolStruct
 from protocol_element import ProtocolElement
 
-class ProtocolPacket(ProtocolElement):
+class ProtocolPacket(ProtocolStruct):
     def __init__(self, protocol, file_name, xml, **kwargs):
         ProtocolElement.__init__(self, protocol, 'packet', file_name, xml,
                                 required_keys=
                                 [
                                     'name'
                                 ])
-        
-    
-    def parse(self, xml):
-        
-        self.data = []
-        
-        for node in xml:
-            if node.tag.lower() == 'data':
-                pass
         
         
     @property
